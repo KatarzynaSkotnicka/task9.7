ef12b57d7bce5ee103202572792b2103f70bdc69
Katarzyna Skotnicka <ka.skotnicka@gmail.com>
2018-01-27 19:33:03 +0100

    Add further js script

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
+//newGame Button
+
 var newGameBtn = document.getElementById('js-newGameButton');
+    newGameBtn.addEventListener('click', newGame);
 
-newGameBtn.addEventListener('click', newGame);
-
-
+//pick possibilities
 var pickRock = document.getElementById('js-playerPick_rock'),
     pickPaper = document.getElementById('js-playerPick_paper'),
     pickScissors = document.getElementById('js-playerPick_scissors');
-
 
 pickRock.addEventListener('click', function() { playerPick('rock') });
 pickPaper.addEventListener('click', function() { playerPick('paper') });
 pickScissors.addEventListener('click', function() { playerPick('scissors') });
 
 
+//game states
 var gameState = 'notStarted',  //started // ended
     player = {
         name: '',
@@ -22,26 +23,21 @@ var gameState = 'notStarted',  //started // ended
         score: 0
     };
 
-
+//set layout elements 
 var newGameElem = document.getElementById('js-newGameElement'),
     pickElem = document.getElementById('js-playerPickElement'),
     resultsElem = document.getElementById('js-resultsTableElement');
 
-
 function setGameElements() {
-
   switch(gameState) {
     case 'started':
         newGameElem.style.display = 'none';
         pickElem.style.display = 'block';
         resultsElem.style.display = 'block';
       break;
-
     case 'ended':
-        newGameBtn.innerText = 'Jeszcze raz';
-
+        newGameBtn.innerText = 'Play again';
     case 'notStarted':
-
     default:
         newGameElem.style.display = 'block';
         pickElem.style.display = 'none';
@@ -51,36 +47,23 @@ function setGameElements() {
 
 setGameElements();
 
-
+//game starts
 var playerPointsElem = document.getElementById('js-playerPoints'),
     playerNameElem = document.getElementById('js-playerName'),
     computerPointsElem = document.getElementById('js-computerPoints');
 
 
 function newGame() {
-  player.name = prompt('Please enter your name', 'imię gracza');
+  player.name = prompt('Please enter your name');
   if (player.name) {
     player.score = computer.score = 0;
     gameState = 'started';
     setGameElements();
 
     playerNameElem.innerHTML = player.name;
-    setGamePoints(); // This function has not been created yet
+    setGamePoints(); 
   }
-
 }
-
-
-function playerPick(playerPick) {
-    var computerPick = getComputerPick();
-
-    playerPickElem.innerHTML = playerPick;
-    computerPickElem.innerHTML = computerPick;
-
-    checkRoundWinner(playerPick, computerPick);
-}
-
-
 
 function getComputerPick() {
     var possiblePicks = ['rock', 'paper', 'scissors'];
@@ -91,6 +74,7 @@ var playerPickElem = document.getElementById('js-playerPick'),
     computerPickElem = document.getElementById('js-computerPick'),
     playerResultElem = document.getElementById('js-playerResult'),
     computerResultElem = document.getElementById('js-computerResult');
+
 function playerPick(playerPick) {
     var computerPick = getComputerPick();
 
@@ -116,15 +100,42 @@ function checkRoundWinner(playerPick, computerPick) {
     if (winnerIs == 'player') {
         playerResultElem.innerHTML = "Win!";
         player.score++;
+        setGamePoints();
     } else if (winnerIs == 'computer') {
         computerResultElem.innerHTML = "Win!";
         computer.score++;
+        setGamePoints();
     }
 
+}
+
+function playerPick(playerPick) {
+    var computerPick = getComputerPick();
+
+    playerPickElem.innerHTML = playerPick;
+    computerPickElem.innerHTML = computerPick;
+
+    checkRoundWinner(playerPick, computerPick);
 }
 
 function setGamePoints() {
     playerPointsElem.innerHTML = player.score;
     computerPointsElem.innerHTML = computer.score;
+    checkRoundWinner();
+}
+
+
+//game ends
+function checkGameWinner() {
+    if (player.score == 10) {
+        alert(player.name + ' wins.');
+        gameState = 'ended';
+        setGameElements();
+        
+    } else if (computer.score == 10) {
+        alert ('Comuter wins');
+        gameState = 'ended';
+        setGameElements();
+    }
 }
 
